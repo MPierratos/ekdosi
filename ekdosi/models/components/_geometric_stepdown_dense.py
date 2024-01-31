b2794ddca68ddec02b4824e560a456e44fbe5958
mpierratos <mpierratos@gmail.com>
2024-01-31 11:32:20 -0500

    add geometric step up

--- ekdosi/models/components/_geometric_stepdown_dense.py
+++ ekdosi/models/components/_geometric_stepdown_dense.py
@@ -1,42 +1,76 @@
+import math
+from typing import Generator, List
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import math
-from typing import List, Generator
 
-def geo(min_n: int, max_n: int, r: float = 2.0) -> Generator[float, None, None]:
-    for iii in range(int(min_n), int(max_n)):
-        if iii == 1:
-            yield r
-        else:
-            yield (-(r ** iii)) / (1 - r)
 
-def generate_geometric_series(min_n: int, max_n: int) -> List[int]:
+def generate_geometric_series(
+    min_n: int, max_n: int, reverse: bool = False
+) -> List[int]:
+    """
+    Iterates over a geometric series.
+
+    min_n: the minimum to start with
+    max_n: the maximum to end at
+    reverse: if the series should be inverted
+
+    i.e. if min_n = 5, and the max_n = 8, then the series would be calculated like so:
+
+    2^5 = 32
+    2^8 = 256
+
+    the output would be:
+    [32, 64, 128, 256] if not reversed
+    [256, 128, 64, 32] if reversed
+    """
+
+    def geo(min_n: int, max_n: int, r: float = 2.0) -> Generator[int, None, None]:
+        """
+        Generate a geometric series.
+        """
+        for iii in range(int(min_n), int(max_n) + 1):
+            yield int(r**iii)
+
     series = list(geo(min_n=min_n, max_n=max_n))
-    series = [int(xxx) for xxx in series]
-    series.reverse()
+    if reverse:
+        series.reverse()
     return series
 
-class GeometricStepdownDense(nn.Module):
+
+class GeometricStepDownDense(nn.Module):
     """Creates a dense set of layers that steps down gradually (by log2)
-    
+
     The dense layers include a linear layer, a dropout layer, and a batch norm layer.
 
     For example: if your input dim is 128, min_n=5 would be 2**5 or 32 output_dim
 
     Therefore, Geometric step down will create dense layers (128->64->32)
         Linear(128)
-        Dropout(0.05)  
+        Dropout(0.05)
         BatchNorm(64)
+
         Linear(64)
+        Dropout(0.05)
+        BatchNorm(64)
+
+        Linear(32)
         Dropout(0.05)
         BatchNorm(32)
     """
-    def __init__(self, input_dim: int, min_n: int = 5, dropout: float = 0.05, batch_norm: bool = True):
-        super(GeometricStepdownDense, self).__init__()
+
+    def __init__(
+        self,
+        input_dim: int,
+        min_n: int = 5,
+        dropout: float = 0.05,
+        batch_norm: bool = True,
+    ):
+        super(GeometricStepDownDense, self).__init__()
         max_n = math.floor(math.log2(input_dim))
         min_n = int(min_n)
-        series = generate_geometric_series(min_n, max_n)
+        series = generate_geometric_series(min_n, max_n, reverse=True)
         self.layers = nn.ModuleList()
         for units in series:
             self.layers.append(nn.Linear(input_dim, units))
@@ -50,15 +84,88 @@ class GeometricStepdownDense(nn.Module):
         for layer in self.layers:
             x = F.relu(layer(x))
         return x
-    
-def test_geometric_stepdown_dense() -> None:
 
-    # Define the input dimension, i.e. input channel is 128 
+
+class GeometricStepUpDense(nn.Module):
+    """Creates a dense set of layers that steps up gradually (by log2)
+
+    The dense layers include a linear layer, a dropout layer, and a batch norm layer.
+
+    For example: if your input dim is 32, max_n=8 would be 2**8 or 256 output_dim
+
+    Therefore, Geometric step up will create dense layers (32->64->128->256)
+        Linear(32)
+        Dropout(0.05)
+        BatchNorm(32)
+
+        Linear(64)
+        Dropout(0.05)
+        BatchNorm(64)
+
+        Linear(128)
+        Dropout(0.05)
+        BatchNorm(128)
+
+        Linear(256)
+        Dropout(0.05)
+        BatchNorm(256)
+    """
+
+    def __init__(
+        self,
+        input_dim: int,
+        max_n: int = 8,
+        dropout: float = 0.05,
+        batch_norm: bool = True,
+    ):
+        super(GeometricStepUpDense, self).__init__()
+        max_n = int(max_n)
+        min_n = math.floor(math.log2(input_dim))
+        series = generate_geometric_series(min_n, max_n, reverse=False)
+        self.layers = nn.ModuleList()
+        for units in series:
+            self.layers.append(nn.Linear(input_dim, units))
+            if dropout is not None:
+                self.layers.append(nn.Dropout(p=dropout))
+            if batch_norm:
+                self.layers.append(nn.BatchNorm1d(units))
+            input_dim = units
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for layer in self.layers:
+            x = F.relu(layer(x))
+        return x
+
+
+def test_geometric_stepdown_dense() -> None:
+    # Define the input dimension, i.e. input channel is 128
     input_dim = 128
 
     # Create an instance of GeometricStepdownDense
     # Because min_n = 5, we know the final layer will have a shape of 2**5, or 32
-    model = GeometricStepdownDense(input_dim=input_dim, min_n=5, dropout=0.05, batch_norm=True)
+    model = GeometricStepDownDense(
+        input_dim=input_dim, min_n=5, dropout=0.05, batch_norm=True
+    )
+
+    # Create a random tensor to represent your input data
+    x = torch.randn(64, input_dim)  # 64 is the batch size
+
+    # Pass the input data through the model
+    output = model(x)
+
+    print(output.shape)
+    print(model)
+
+
+def test_geometric_stepup_dense() -> None:
+    # Define the input dimension, i.e. input channel is 32
+    input_dim = 32
+
+    # Create an instance of GeometricStepdownDense
+    # Because max_n = 8, we know the final layer will have a shape of 2**8, or 256
+    model = GeometricStepUpDense(
+        input_dim=input_dim, max_n=8, dropout=0.05, batch_norm=True
+    )
 
     # Create a random tensor to represent your input data
     x = torch.randn(64, input_dim)  # 64 is the batch size
--- ekdosi/models/components/_temporal_components.py
+++ ekdosi/models/components/_temporal_components.py
@@ -1,11 +1,12 @@
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
+
 
 class DifferenceLayer(nn.Module):
     """Assumes <batch>, <time>, <channels>,
-    
-    This difference layer is a non-trainable that takes the difference between 
+
+    This difference layer is a non-trainable that takes the difference between
     parameters across the time dimension. This is useful for building at first and
     second derivatives.
     """
@@ -14,14 +15,13 @@ class DifferenceLayer(nn.Module):
         super(DifferenceLayer, self).__init__()
         self.use_float64 = use_float64
         self.diff_convolution = nn.Conv2d(
-            in_channels=1,
-            out_channels=1,
-            kernel_size=(2,1),
-            bias=False
+            in_channels=1, out_channels=1, kernel_size=(2, 1), bias=False
         )
 
-        self.diff_convolution.weight.requires_grad=False
-        self.diff_convolution.weight.data = self.kernel_initializer(self.diff_convolution.weight.shape)
+        self.diff_convolution.weight.requires_grad = False
+        self.diff_convolution.weight.data = self.kernel_initializer(
+            self.diff_convolution.weight.shape
+        )
 
     def kernel_initializer(self, shape):
         """initializes the kernel to that creates a diff across a direction"""
@@ -33,15 +33,15 @@ class DifferenceLayer(nn.Module):
     def forward(self, input):
         input = input.unsqueeze(1)
         return self.diff_convolution(input)
-    
+
 
 def test_DifferenceLayer():
     def checkerboard(shape):
         return np.indices(shape).sum(axis=0) % 2
-    
+
     df = torch.tensor(checkerboard(shape=(3, 7, 5)), dtype=torch.float32)
     correct = torch.diff(df, dim=1)
-   
+
     d1 = DifferenceLayer()
     res = torch.squeeze(d1(df))
     assert res.dtype == correct.dtype
@@ -50,4 +50,3 @@ def test_DifferenceLayer():
         return True
     else:
         return False
-
